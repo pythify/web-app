00cf8ae83eacc76da82b3b9b8eb0d97700ea364f
Juan Pablo Calao <jcalaop@gmail.com>
2020-07-25 13:14:22 -0500

    add required param to input fields

--- src/components/molecules/FormField/FormField.jsx
+++ src/components/molecules/FormField/FormField.jsx
@@ -3,11 +3,14 @@ import PropTypes from 'prop-types'
 import { FormInput } from '../../atoms'
 import styles from './FormField.module.scss'
 
-export function FormField({ label, type, id, ...rest }) {
+export function FormField({ label, type, id, required, ...rest }) {
 	return (
 		<div className={styles['FormField']}>
-			<label htmlFor={id}>{label}</label>
-			<FormInput variant={type} name={id} {...rest} />
+			<label htmlFor={id}>
+				{label}
+				{required && ' *'}
+			</label>
+			<FormInput variant={type} name={id} required={required} {...rest} />
 		</div>
 	)
 }
