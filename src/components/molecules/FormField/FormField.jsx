36c9873b1d430eb716c6ddfa5680b196e9a5cc76
Juan Pablo Calao <jcalaop@gmail.com>
2020-07-25 11:39:49 -0500

    add rest props to form inputs

--- src/components/molecules/FormField/FormField.jsx
+++ src/components/molecules/FormField/FormField.jsx
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types'
 import { FormInput } from '../../atoms'
 import styles from './FormField.module.scss'
 
-export function FormField({ label, type, id }) {
+export function FormField({ label, type, id, ...rest }) {
 	return (
 		<div className={styles['FormField']}>
 			<label htmlFor={id}>{label}</label>
-			<FormInput variant={type} name={id} />
+			<FormInput variant={type} name={id} {...rest} />
 		</div>
 	)
 }
