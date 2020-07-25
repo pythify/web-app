2b8d935f23ac42f02a9dff5d0d58f7dddd658cb1
Juan Pablo Calao <jcalaop@gmail.com>
2020-07-25 13:10:04 -0500

    style form component

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { RegisterForm } from '../components/organisms'
 import { LogoTemplate } from '../templates'
+import styles from '../scss/pages/RegisterPage.module.scss'
 
 export function RegisterPage() {
 	return (
 		<LogoTemplate>
-			<RegisterForm />
+			<div className={styles['RegisterPage']}>
+				<figure className={styles['RegisterPage__banner']}>
+					<img src="" alt="Banner" />
+				</figure>
+				<RegisterForm />
+			</div>
 		</LogoTemplate>
 	)
 }
