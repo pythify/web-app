513d50d5579215f9eb7efc08143eac84e90793a8
den <den.velez@gmail.com>
2020-08-06 10:55:25 -0500

    private route without logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,45 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './scss/global.scss';
-import { RegisterPage, LoginPage } from './pages';
+import { RegisterPage, LoginPage, Library, NotFound } from './pages';
 
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false });
+};
 export function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/register">
-          <RegisterPage />
-        </Route>
-        <Route path="/login">
-          <LoginPage />
-        </Route>
-      </Switch>
-    </Router>
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <RegisterPage />
+          </Route>
+          <Route exact path="/register">
+            <RegisterPage />
+          </Route>
+          <Route exact path="/login">
+            <LoginPage />
+          </Route>
+          <Route exact path="/library">
+            <UserLogged>
+              {({ isAuth }) =>
+                isAuth ? (
+                  <Route exact path="/library">
+                    <Library />
+                  </Route>
+                ) : (
+                  <Route exact path="/library">
+                    <LoginPage />
+                  </Route>
+                )
+              }
+            </UserLogged>
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
---
+++ src/pages/Library.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Library() {
+  return <h1>this is the library</h1>;
+}
