a4a99b9bf61831bab083610a2c751bf6e254eb2d
Juan Pablo Calao <jcalaop@gmail.com>
2020-08-10 16:47:34 -0500

    fix production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = function webpackConfig(env) {
   const plugins = [
     new HtmlWebpackPlugin({
       template: './public/index.html'
     })
-  ]
+  ];
 
   if (env.NODE_ENV === 'production') {
-    plugins.push(new CleanWebpackPlugin())
+    plugins.push(new CleanWebpackPlugin());
+    plugins.push(new MiniCssExtractPlugin());
   }
 
   return {
@@ -84,5 +85,5 @@ module.exports = function webpackConfig(env) {
     optimization: {
       minimizer: [new OptimizeCSSAssetsPlugin({})]
     }
-  }
-}
+  };
+};
