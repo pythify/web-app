f75e0be11fb6977e4489a63949fd951b529fdbac
Juan Pablo Calao <jcalaop@gmail.com>
2020-08-11 23:07:57 -0500

    add player

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './scss/global.scss';
 import { RegisterPage, LoginPage, Library, NotFound } from './pages';
 
-const UserLogged = ({ children }) => {
-  return children({ isAuth: false });
-};
+const UserLogged = ({ children }) => children({ isAuth: true });
+
 export function App() {
   return (
     <>
@@ -22,17 +21,20 @@ export function App() {
           </Route>
           <Route exact path="/library">
             <UserLogged>
-              {({ isAuth }) =>
-                isAuth ? (
-                  <Route exact path="/library">
-                    <Library />
-                  </Route>
-                ) : (
+              {({ isAuth }) => {
+                if (isAuth) {
+                  return (
+                    <Route exact path="/library">
+                      <Library />
+                    </Route>
+                  );
+                }
+                return (
                   <Route exact path="/library">
                     <LoginPage />
                   </Route>
-                )
-              }
+                );
+              }}
             </UserLogged>
           </Route>
           <Route>
---
+++ src/components/organisms/Player/Player.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export function Player({ url }) {
+  return (
+    <div>
+      <audio controls src={url} />
+    </div>
+  );
+}
+
+Player.propTypes = {
+  url: PropTypes.string
+};
+
+Player.defaultProps = {
+  url: ''
+};
